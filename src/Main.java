0655db9492c08a81b94a032edb4297710532196c
Nathy Rodrigues <nathalia.r.s@outlook.com>
2022-07-23 17:20:07 -0300

    criando classe curso e mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso Javascript");
+        curso2.setDescricao("descrição curso Javascript");
+        curso2.setCargaHoraria(5);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+              }
 
     }
-}
